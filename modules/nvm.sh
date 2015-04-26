74c987f5ea8b1faaada706849797d15bf7b7e5be
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2015-04-26 19:09:14 +0200

    Fix for nvm executable not being know to the shell

--- modules/nvm.sh
+++ modules/nvm.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
 
-nvm install 0.11
-nvm install 0.12
-nvm alias stable 0.12
-nvm alias default 0.11
-nvm use 0.11
+# nvm sources in the .bashrc by default. If .bash_profile
+# is available, this if sources it in the .bash_profile instead.
+# You still need to manually remove it from .bashrc though…
+if [[ -f ~/.bash_profile ]]; then
+  echo "export NVM_DIR='/Users/$(whoami)/.nvm'" >> ~/.bash_profile
+  echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> ~/.bash_profile
+fi
+
+# As long as the shell is not loaded `nvm` is unknown to the shell
+# The shell will restart after worker is finished.
+# For now they are run directly from the shell script in the nvm directory.
+$NVM_DIR/nvm.sh install 0.11
+$NVM_DIR/nvm.sh install 0.12
+$NVM_DIR/nvm.sh alias stable 0.12
+$NVM_DIR/nvm.sh alias default 0.11
+$NVM_DIR/nvm.sh use 0.11
