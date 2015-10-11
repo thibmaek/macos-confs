0bf5ec105a21a0b48a1b340de83967141e0706b0
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2015-10-11 12:29:11 +0200

    Rbenv for ruby and otto as vagrant replacement

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# make --user-install a default so gem install
-# doesn't require sudo
-echo "gem: --user-install" > ~/.gemrc
+# Using rbenv over OS X pre-installed Ruby in Brewfile
+rbenv install 2.2.2
+rbenv global 2.2.2
+
+echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.bash_profile
+echo "eval \"$(rbenv init -)\"" >> ~/.bash_profile
+echo "if which rbenv > /dev/null; then eval \"$(rbenv init -)\"; fi" >> ~/.bash_profile
+
+echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc
