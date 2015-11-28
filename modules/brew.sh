01f70620e8aeed8a6ea3a2f713d7bf1adc4f89e6
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2015-11-29 00:54:19 +0100

    Fixed bugs
    My if structures were faulty but have now been corrected with the right
    check for binaries before installing.

--- modules/brew.sh
+++ modules/brew.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # 1. Check if xcode tools are installed and install brew
-if ! [[ which xcode-select ]]; then
-  echo "Command line tools not installed!"
-  return
-else
+if which xcode-select >/dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew doctor
+else
+  echo "Command line tools not installed!"
+  exit 0
 fi
 
 # 2. Tap boneyard for Brewfile support
-if ! [[ which brew ]]; then
-  echo "brew not installed/found!"
-  return
-else
+if which brew >/dev/null; then
   brew tap Homebrew/bundle
+else
+  echo "brew not installed/found!"
+  exit 0
 fi
