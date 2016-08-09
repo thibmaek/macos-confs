474b2b8c4ef83e40e901601618fe8a3f6ef0cdee
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-08-09 20:46:05 +0200

    Added bundler completion

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -11,3 +11,5 @@ echo "if which rbenv > /dev/null; then eval \"$(rbenv init -)\"; fi" >> ~/.bash_
 echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc
 
 gem install bundler
+gem install bundler_bash_completion
+echo "eval \"$(complete_bundle_bash_command init)\"" >> ~/.bash_profile
