848b9734c205fedb96f887059430e654b1f7d021
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2016-08-26 18:07:19 +0200

    Fixes for nvm.test.sh
    An incorrect constructed if conditional

--- tests/nvm.test.sh
+++ tests/nvm.test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 local NPMTEST=../extensions/npm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | PROFILE=$HOME/.bash_profile bash
 
-if [[ ! -f $HOME/.bash_profile && ! -d $HOME/.nvm/]]; then
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | PROFILE=$HOME/.bash_profile bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+if [ ! -f $HOME/.bash_profile && ! -d $HOME/.nvm/]; then
   exit 1
 else
   if !which npm >/dev/null; then exit 1; fi
