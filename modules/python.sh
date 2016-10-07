4d5d3f009b58bd9f4f673ec49271872e01a7ffad
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-10-07 22:09:21 +0200

    Fixes for rbenv & pyenv initializing

--- modules/python.sh
+++ modules/python.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "You should run brew info pyenv and read the caveats about putting the eval() in your profile"
+eval "$(pyenv init -)"
+
 pyenv update
 pyenv install 2.7.10
+pyenv shell 2.7.10
 pyenv global 2.7.10
