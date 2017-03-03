829b5bd96cd2fb7da0034101a1d081ee5629751f
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-03-03 01:00:22 +0100

    :art: Improved commandline tools check. Tap bundle by default

--- modules/brew.sh
+++ modules/brew.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
 # 1. Check if xcode tools are installed and install brew
-if which xcode-select >/dev/null; then
+if make --version >/dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew doctor
-else
-  echo "Command line tools not installed!" && exit 1
-fi
-
-# 2. Tap boneyard for Brewfile support
-if which brew > /dev/null; then
   brew tap Homebrew/bundle
 else
-  echo "brew not installed/found!" && exit 1
+  echo "Command line tools not installed!" && exit 1
 fi
