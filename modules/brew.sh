cc273f145c4ed13a2925331f1b2c6f9eeeebf054
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-06-15 16:22:13 +0200

    🎨 Use a function to do brew install checks

--- modules/brew.sh
+++ modules/brew.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+function install_brew() {
+  if which brew > /dev/null; then
+    echo "brew already installed..."
+  else
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+    brew tap Homebrew/bundle
+  fi
+}
+
 # 1. Check if xcode tools are installed and install brew
 if make --version >/dev/null; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew doctor
-  brew tap Homebrew/bundle
+  install_brew
 else
   echo "Command line tools not installed!" && exit 1
 fi
