42a530a0b5bddeb63c98bcc3676d39c07657ebe6
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-11-24 22:03:39 +0100

    Remove unknown subcommand update for pyenv

--- modules/python.sh
+++ modules/python.sh
@@ -3,7 +3,6 @@
 echo "You should run brew info pyenv and read the caveats about putting the eval() in your profile"
 eval "$(pyenv init -)"
 
-pyenv update
 pyenv install 3.6.0
 pyenv install 2.7.12
 pyenv global 2.7.12
