11c041b2bd818d37d3cba108e599aeb18cc9706b
thibmaek <thibault.maekelbergh@icloud.com>
2018-08-19 11:01:43 +0200

    Add installer script for powerline. Casing for filename for powerline-fonts

---
+++ configs/powerline/install-powerline.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function pretty_print() {
+  printf "\n\033[1m\033[34m%s\033[0m\n\n" "[macos-confs] ${1}…"
+}
+
+function install_powerline_fonts() {
+  pip install --upgrade powerline-shell
+}
+
+if which pip > /dev/null; then
+  install_powerline;
+else
+  pretty_print "❗️ pip is required for powerline"
+  pretty_print "You can install pip yourself or run installers/install-python.sh"
+  exit 1
+fi
