0976ef712f657b6699e4153b2404febb76ff7110
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-10-14 19:48:48 +0200

    Use computed brew dir i/o /usr/bin

--- installers/install-brew-bash.sh
+++ installers/install-brew-bash.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BREW_PREFIX=$(brew --prefix)
+
 function pretty_print() {
   printf '\n\033[1m\033[34m%s\033[0m\n\n' "[macos-confs] ${1}…"
 }
@@ -11,9 +13,9 @@ function install_brew_bash() {
   brew install bash
 
   pretty_print "⚠️ Changing from BSD bash (macOS) to GNU bash (brew)"
-  if ! grep -q '/usr/local/bin/bash' /etc/shells; then
-    echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-    chsh -s /usr/local/bin/bash;
+  if ! grep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+    chsh -s "${BREW_PREFIX}/bin/bash";
 
     pretty_print "Auto-reloading your shell by executing a login shell"
     exec "$SHELL" -l
